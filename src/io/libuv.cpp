422f3800bc7f5eb6b2e92c2ba470f49d6d5cda0d
张 骞 <zq_sdk@163.com>
2017-06-10 20:22:34 +0800

    完成server

--- src/io/libuv.cpp
+++ src/io/libuv.cpp
@@ -1,6 +1,6 @@
 #include <cassert>
 #include "libuv.h"
-
+using namespace std;
 
 uv_loop_t *loop;
 struct sockaddr_in addr;
@@ -13,13 +13,32 @@ void read(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
 }
 
 // 发送数据
+void write(uv_write_t *req, int status) {  
+    if (status) fprintf(stderr, "Write error %s\n", uv_strerror(status));
+    free(req);  
+} 
+
+// 发送数据
 void send(uv_stream_t *client, ssize_t nread, const uv_buf_t *buff) {
+  
   if(nread < 0) {
     if(nread != UV_EOF) fprintf(stderr, "read error %s \n", uv_err_name(nread));
     uv_close((uv_handle_t*) client, NULL);
-  }
+  } else {
+		uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t)); 
+    //uv_buf_t wrbuf = uv_buf_init(buff->base, nread);
+
+    
+    // 转发到交易中心
+    char *futures_str = (new routes(buff->base))->data;
+
+    uv_buf_t wrbuf = uv_buf_init(futures_str, strlen(base));
+    
+    uv_write(req, client, &wrbuf, 1, write);
+	}
 
   printf("echo_read:%s\r\n", buff->base);
+  
   if(buff->base) free(buff->base);
 
 }
--- src/io/libuv.h
+++ src/io/libuv.h
@@ -24,15 +24,6 @@ class IIOService {
     // 停止本地服务/交易中心服务连接
     //void stop();
 
-  private :
-    
-    // TUDO: 设置本地/交易中心服务监听地址/端口
-    const char *host = "0.0.0.0";
-    const int port = 7000;
-
-    const char *host_ftp = "0.0.0.0";
-    const int port_ftp = 7000;
-
 };
 
 #endif
