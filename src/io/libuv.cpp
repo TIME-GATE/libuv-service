3612f3a2bc5defe4505d87b5cc451946017d642c
dreamboad <zq_sdk@163.com>
2017-06-10 18:30:47 +0800

    have done

--- src/io/libuv.cpp
+++ src/io/libuv.cpp
@@ -1,27 +1,19 @@
 #include <cassert>
-
 #include "libuv.h"
 
-int IIOService::listen(const char *host, int port) {
-	printf("######## start ftp service HOST: %s PORT: %d ################### \n", host, port);  
-	
-	loop = uv_default_loop(); 
-  uv_tcp_t server;  
-  uv_tcp_init(loop, &server);  
-  uv_ip4_addr(host, port, &addr); 
-  uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
-  
-	int listen_status = uv_listen((uv_stream_t*) &server, default_backlog, IIOService::onConnect);   
-	if (listen_status) {   
-  	fprintf(stderr, "Listen error %s\n", uv_strerror(listen_status));  
-  	return 1;  
-  }   
-  
-	return uv_run(IIOService::loop, UV_RUN_DEFAULT);
 
+uv_loop_t *loop;
+struct sockaddr_in addr;
+const int  default_backlog = 128;
+
+// 申请连接内存
+void read(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+  buf->base = (char*) malloc(suggested_size);  
+  buf->len = suggested_size; 
 }
 
-void IIOService::send(uv_stream_t *client, ssize_t nread, const uv_buf_t *buff) {
+// 发送数据
+void send(uv_stream_t *client, ssize_t nread, const uv_buf_t *buff) {
   if(nread < 0) {
     if(nread != UV_EOF) fprintf(stderr, "read error %s \n", uv_err_name(nread));
     uv_close((uv_handle_t*) client, NULL);
@@ -32,26 +24,42 @@ void IIOService::send(uv_stream_t *client, ssize_t nread, const uv_buf_t *buff) 
 
 }
 
-void IIOService::onConnect(uv_stream_t *server, int status) {
-	if (status < 0) {  
-  	fprintf(stderr, "one New connection error %s\n", uv_strerror(status));  
+void onConnect(uv_stream_t *server, int status) {
+  if (status < 0) {   
+    fprintf(stderr, "one New connection error %s\n", uv_strerror(status));  
     return;  
-  }  
-      
+  }   
+        
   uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));  
-  uv_tcp_init(IIOService::loop, client);  
+  uv_tcp_init(loop, client);  
   
-	if (uv_accept(server, (uv_stream_t*) client) == 0) {  
-  	uv_read_start((uv_stream_t*) client, IIOService::read, IIOService::send);  
-  } else {  
-  	uv_close((uv_handle_t*) client, NULL);  
-  }  
+  if (uv_accept(server, (uv_stream_t*) client) == 0) {   
+    uv_read_start((uv_stream_t*) client, read, send);  
+  } else {   
+    uv_close((uv_handle_t*) client, NULL);  
+  }   
   
-	printf("one new connection have been OK, status:%d\r\n", status);   
+  printf("one new connection have been OK, status:%d\r\n", status);   
 
 }
 
-void IIOService::read(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
-  buf->base = (char*) malloc(suggested_size);  
-  buf->len = suggested_size; 
+
+int IIOService::listen(const char *host, int port) {
+	printf("######## start ftp service HOST: %s PORT: %d ################### \n", host, port);  
+	
+	loop = uv_default_loop(); 
+  uv_tcp_t server;  
+  uv_tcp_init(loop, &server);  
+  uv_ip4_addr(host, port, &addr); 
+  uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
+  
+	int listen_status = uv_listen((uv_stream_t*) &server, default_backlog, onConnect);   
+	if (listen_status) {   
+  	fprintf(stderr, "Listen error %s\n", uv_strerror(listen_status));  
+  	return 1;  
+  }   
+  
+	return uv_run(loop, UV_RUN_DEFAULT);
+
 }
+
--- src/io/libuv.h
+++ src/io/libuv.h
@@ -5,21 +5,13 @@
 #include <string>
 #include <uv.h>
 
-// 会话列表
-//typedef std::vector<sid_t> SidList
-
 // 网络通信层
 class IIOService {
 
   public : 
 
-    IIOService();
-    virtual ~IIOService ();
-
-  public :
-   
-    // 接受连接事件, 定义连接回调
-    static void onConnect(uv_stream_t *server, int status);
+    IIOService() {};
+    virtual ~IIOService () {};
   
   public :
 
@@ -27,16 +19,10 @@ class IIOService {
     int listen(const char *host, int port);
     
     // 连接期货交易中心
-    int connect(const char *host, uint16_t port, int32_t seconds);
+    //int connect(const char *host, uint16_t port, int32_t seconds);
 
     // 停止本地服务/交易中心服务连接
-    void stop();
-    
-    // 申请连接内存
-    static void read(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf);
-
-    // 发送数据
-    static void send(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf);
+    //void stop();
 
   private :
     
@@ -46,12 +32,6 @@ class IIOService {
 
     const char *host_ftp = "0.0.0.0";
     const int port_ftp = 7000;
-  
-  private :
-
-  static uv_loop_t *loop;
-  struct sockaddr_in addr;
-  const int  default_backlog = 128;
 
 };
 
