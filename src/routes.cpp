e36bb7cc75c417e91c043b13ad260c808af851d7
dreamboad <zq_sdk@163.com>
2017-06-10 22:12:43 +0800

    done trader

--- src/routes.cpp
+++ src/routes.cpp
@@ -1,11 +1,15 @@
-#include "./sdk/ThostFtdcMdApi.h"
+#include "./sdk/ThostFtdcTraderApi.h"
+#include "./sdk/ChildThostFtdcTraderApi.h"
 
 int main() {
-  CThostFtdcMdApi * api = CThostFtdcMdApi::CreateFtdcMdApi("/Users/dreamboad/Projects/libuv-service/data", true);
+  CThostFtdcTraderApi * api = CThostFtdcTraderApi::CreateFtdcTraderApi("/Users/dreamboad/Projects/libuv-service/data");
   MarketDataCollector mdCollector(api);
 
   api->RegisterSpi(&mdCollector);
   api->RegisterFront("tcp://127.0.0.1:17001");
+  THOST_TE_RESUME_TYPE one;
+  api->SubscribePrivateTopic(one);
+  api->SubscribePublicTopic(one);
   api->Init();
   api->Join();
   return 1;
