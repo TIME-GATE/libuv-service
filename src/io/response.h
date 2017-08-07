8754f872d38b8cb2987fe5055b00d28e624622af
张 骞 <zq_sdk@163.com>
2017-08-07 17:43:39 +0800

    添加项目管理工具

--- src/io/response.h
+++ src/io/response.h
@@ -16,21 +16,21 @@ class Response {
   
   public :
 
-    // 申请连接内存
+    // 读数据
     static void read(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf);   	
  
     // 写数据
     static void write(uv_write_t *req, int status);
-
-		// 读数据
+    
+    // 发送数据
     static void send(uv_stream_t *client, ssize_t nread, const uv_buf_t *buff);	
     
-		// 监听新链接
+    // 监听新链接
     static void onConnect(uv_stream_t *server, int status);
     
   public : 
-		
-		// 事件句柄
+  
+    // 事件句柄
     static uv_loop_t *loop;
 
     // 服务
